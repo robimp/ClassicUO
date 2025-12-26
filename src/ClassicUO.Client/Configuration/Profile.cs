4c07c03da312c088e4996dbf5f36afa0c942a470
Robim Pacheco <robim.poa@gmail.com>
2025-12-26 03:37:55 -0300

    Default for AuraOnMouse set to false

--- src/ClassicUO.Client/Configuration/Profile.cs
+++ src/ClassicUO.Client/Configuration/Profile.cs
@@ -236,7 +236,7 @@ namespace ClassicUO.Configuration
         public bool ShadowsStatics { get; set; } = true;
         public int TerrainShadowsLevel { get; set; } = 15;
         public int AuraUnderFeetType { get; set; } // 0 = NO, 1 = in warmode, 2 = ctrl+shift, 3 = always
-        public bool AuraOnMouse { get; set; } = true;
+        public bool AuraOnMouse { get; set; } = false;
         public bool AnimatedWaterEffect { get; set; } = false;
 
         public bool PartyAura { get; set; }
